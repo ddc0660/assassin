436b35ed716feed982cf2773acbde1dd2d0eb6c4
David Campbell <david.d.campbell@gmail.com>
2014-01-12 02:38:48 -0500

    added in an interesting experiment to track position frequently

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,32 +5,44 @@
     var webServiceURL = 'http://1.code-newyears-2013.appspot.com/';
 
     angular.module('assassinApp.controllers', []).
-    controller('LoginCtrl', function ($scope, $http) {
-        $http.jsonp(webServiceURL + '?callback=JSON_CALLBACK').
-        success(function (data) {
-            $scope.response = data;
-        }).
-        error(function (data) {
-            console.error('HTTP GET failed');
-        });
-        
-        $scope.email = localStorage.getItem('email');
-        $scope.name = localStorage.getItem('name');
-        
-        $scope.loggedIn = ($scope.email !== null && $scope.name !== null);
-        
-        $scope.login = function(name, email) {
-            localStorage.setItem('name', name);
-            localStorage.setItem('email', email);
-            $scope.loggedIn = true;
-        };
-        
-        $scope.logOff = function() {
-            localStorage.removeItem('name');
-            localStorage.removeItem('email');
-            $scope.loggedIn = false;
-        };
-    })
+    controller('LoginCtrl', 
+               
+               function ($scope, $http) {
+                   
+                    $scope.isNetworkAvailable = 'Internet Access Enabled';
+                   
+                    if(navigator.network.connection.type == Connection.NONE) {
+                        $scope.isNetworkAvailable = 'No Internet Access';
+                    } 
+                   
+                   
+                    // this ping is just to hit the server for no real reason
+                    // TODO: refactor this out for an actual attempt to connect to server    
+                    $http.jsonp(webServiceURL + '?callback=JSON_CALLBACK').
+                    success(function (data) {
+                        $scope.response = data;
+                    }).
+                    error(function (data) {
+                        console.error('HTTP GET failed');
+                    });
+                    
+                    $scope.email = localStorage.getItem('email');
+                    $scope.name = localStorage.getItem('name');
+                    
+                    $scope.loggedIn = ($scope.email !== null && $scope.name !== null);
+                    
+                    $scope.login = function(name, email) {
+                        localStorage.setItem('name', name);
+                        localStorage.setItem('email', email);
+                        $scope.loggedIn = true;
+                    };
+                    
+                    $scope.logOff = function() {
+                        localStorage.removeItem('name');
+                        localStorage.removeItem('email');
+                        $scope.loggedIn = false;
+                    };
+                })
     .controller('LocationCtrl', function ($scope, $http, geolocation) {
         console.log('LocationCtrl');
 
@@ -44,14 +56,7 @@
               'Heading: ' + position.coords.heading           + '\n' +
               'Speed: ' + position.coords.speed             + '\n' +
               'Timestamp: ' + position.timestamp                + '\n');
-            $scope.p = 'Latitude: ' + position.coords.latitude + '<br />' +
-                        'Longitude: ' + position.coords.longitude + '<br />' + 
-                        'Altitude: ' + position.coords.altitude + '<br />' +
-                        'Accuracy: ' + position.coords.accuracy + '<br />' +
-                        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
-                        'Heading: ' + position.coords.heading + '<br />' +
-                        'Speed: ' + position.coords.speed + '<br />' +
-                        'Timestamp: ' + position.timestamp + '<br />';
+            $scope.position = position;
             sessionStorage.setItem('longitude', position.coords.longitude);
             sessionStorage.setItem('latitude', position.coords.latitude);
             JoinGame($http);
@@ -72,7 +77,63 @@
             FindNearby($http);
         };
             
-
+        $scope.startTracking = function() {
+            var tracking_data = [];
+            var watch_id = navigator.geolocation.watchPosition(
+                // Success
+                function(position) {
+                    tracking_data.push(position);
+                },
+                // Error
+                function(error) {
+                    console.log(error);
+                },
+                // Settings
+                { frequency: 3000, enableHighAccuracy: true });
+        };
+        
+        $scope.stopTracking = function() {
+            // Stop tracking the user
+            navigator.geolocation.clearWatch(watch_id);
+	
+            // Save the tracking data
+            //localStorage.setItem(track_id, JSON.stringify(tracking_data));
+            
+            // Set the initial Lat and Long of the Google Map
+            var myLatLng = new google.maps.LatLng(tracking_data[0].coords.latitude, tracking_data[0].coords.longitude);
+        
+            // Google Map options
+            var myOptions = {
+                zoom: 15,
+                center: myLatLng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+        
+            // Create the Google Map, set options
+            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        
+            var trackCoords = [];
+            
+            // Add each GPS entry to an array
+            for(var i=0; i<tracking_data.length; i++){
+                trackCoords.push(new google.maps.LatLng(tracking_data[i].coords.latitude, tracking_data[i].coords.longitude));
+            }
+            
+            // Plot the GPS entries as a line on the Google Map
+            var trackPath = new google.maps.Polyline({
+                path: trackCoords,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+        
+            // Apply the line to the map
+            trackPath.setMap(map);
+            
+            // Reset watch_id and tracking_data 
+            var watch_id = null;
+            var tracking_data = null;
+        };
             
         console.log('leaving LocationCtrl');
     });
